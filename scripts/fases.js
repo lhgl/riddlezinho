b4812e5f7c4472e2853b0ea03f7687a9103702ff
victor-rocha <victor.rocha93@outlook.com>
2020-09-11 00:33:57 +0200

    campo de senha parcialmente funcionando. correcao do warning do chrome

--- scripts/fases.js
+++ scripts/fases.js
@@ -1,7 +1,17 @@
 function limparString(e) {
-    
+    const character = String.fromCharCode(e.keyCode);
+    if (!(/^[a-zA-Z0-9]+$/igm).test(character)) {
+        console.log("entrou")
+        e.preventDefault();
+        return false;
+    }
+    if (e.keyCode === 13)
+        passarDeFase();
 }
 
 function passarDeFase() {
-    
+    const senha = document.getElementById("senha").value;
+    if (senha === '') return;
+    const url = window.location.href;
+    window.location.href = `${url.substring(0, url.lastIndexOf("/"))}/${senha.toLowerCase()}.html`;
 }
